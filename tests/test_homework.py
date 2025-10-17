69201417121f6e510d04b79be9559449594d9472
Victor Velásquez <vvelasquez@safefleet.net>
2025-10-17 16:15:32 -0500

    PRE-19

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -6,6 +6,6 @@ import os
 def test_01():
     """Test app"""
 
-    assert os.path.exists("homework/inventory.db")
+    assert os.path.exists("app/inventory.db")
     assert os.path.exists("homework/main.py")
     assert os.path.exists("homework/db_setup.py")
